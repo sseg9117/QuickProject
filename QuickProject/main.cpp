35be0d05e9753c2e012882e2b7f80f39bf6c3082
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-01-25 08:21:50 -0700

    Initial commit
    first commit

---
+++ QuickProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickProject
+//
+//  Created by Segota, Sheradon on 1/25/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
